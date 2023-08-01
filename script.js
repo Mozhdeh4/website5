ee83a9bedb5f6b2f12942e869c20288377f982d9
Mozhdeh4 <140979635+Mozhdeh4@users.noreply.github.com>
2023-08-01 23:42:42 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active'); 
+    searchForm.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active'); 
+    cartItem.classList.remove('active');
+}
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active'); 
+}
